0ff4e5923897d188d2890db8f165a1caa22f8cfb
JuanEs-az <juaneste687@gmail.com>
2023-01-25 11:51:19 -0500

    Something fixed

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,10 @@ let btnComenzar = document.querySelector(".btn-comenzar");
 btnComenzar.addEventListener("click", () => {
     let logo = document.querySelector(".logo");
     let imgs = {
-        "paperairplane": "./public/img/avion-de-papel.png",
-        "airplane":      "./public/img/avion.png",
+        "paperairplane": "https://res.cloudinary.com/dvaqrycs1/image/upload/v1674066798/avion-de-papel_nefure.png",
+        "airplane":      "https://res.cloudinary.com/dvaqrycs1/image/upload/v1674066798/avion_nhvkbz.png",
     };
-    if( logo.src.includes('avion-de-papel.png') ){
+    if( logo.src.includes('papel_nefure') ){
         logo.src = imgs["airplane"];   
     }else{
         logo.src = imgs["paperairplane"];
